0dca3c51ae1e7e990072dd6c53d5948082952abc
MangalaRajbhar18 <mangalarajbhar18@gmail.com>
2026-06-04 15:02:21 +0530

    Added CORS configuration

---
+++ src/main/resources/CorsConfig.java
@@ -0,0 +1,32 @@
+
+package com.rsps.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://superb-cascaron-798d76.netlify.app"
+                        )
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        )
+                        .allowedHeaders("*");
+            }
+        };
+    }
